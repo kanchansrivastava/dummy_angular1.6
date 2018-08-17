dcb2df86a9ca3c313b04ac86e61292e3d0ccee49
Kanchan Srivastava <Kanchan.Srivastava@cdk.com>
2018-08-17 15:02:47 +0530

    added autocomplete

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,25 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
-
+var view1 = angular.module('myApp.view1', ['ngRoute', 'ui.bootstrap'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
   });
-}])
+}]);
 
-.controller('View1Ctrl', [function() {
 
+view1.controller('View1Ctrl', [
+    '$scope', '$http', function($scope, $http) {
+  $scope.selectedCompany = '';
+
+  $scope.getCompanies = function(searchKeyword) {
+    return $http.get('https://daas-qa-sig-api.circleback.com/service/contactcloud/companies/autocomplete', {
+      params: {
+        company_name: searchKeyword
+      }
+    }).then(function(response){
+        return response.data.searchResults
+    });
+  };
 }]);
